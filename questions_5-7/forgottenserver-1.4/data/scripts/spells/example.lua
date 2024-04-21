42b438ed6fbb0962d0623984c74673805cd8e54f
Chris R <chrisregan50@gmail.com>
2024-04-21 06:52:26 -0500

    Largly finished question 5
    Just missing adding the damage effect to the spell.  Ended up being an offset value for when the position x and y value are both either even or odd.

--- questions_5-7/forgottenserver-1.4/data/scripts/spells/example.lua
+++ questions_5-7/forgottenserver-1.4/data/scripts/spells/example.lua
@@ -1,220 +1,108 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 
 
---Facing South
---[[local area = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}--]]
+local combat={}
 
-local areaEvent1 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 0, 0, 0, 0, 0},
-	{ 1, 1, 0, 2, 0, 0, 1},
-	{ 1, 1, 0, 0, 0, 0, 1},
-	{ 0, 1, 1, 0, 0, 0, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
 
-local areaEvent2 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 1, 1, 0, 2, 0, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 0, 0, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
+-- Initialize the 4 areas needed for the visuals.
+-- Seperated by the minimum required tables spawned in at a time.
 
-local areaEvent3 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
+-- 1 = damage area
+-- 2 = player origin
+-- 3 = damage area and player origin
 
-local areaEvent4 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 0, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
+-- Direction from origin on table to ingame directions.
+-- Up = South
+-- Right = West
+-- Left = East
+-- Down = North
 
-local areaEvent5 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
+-- areas are in a table to more easily relate it to the combat objects they are helping to initialize.
+-- Two seperate tables since if both x and y coord are even or odd has a different animation Than
+-- X being even or odd and y being even or odd the opposite of x.
+local MixedOddEven ={
+				-- First table.  Spawns 3 large tornadoes and 3 smaller tornades
+				{
+					{0, 1, 0, 0, 0, 0, 0},
+					{1, 0, 0, 2, 0, 0, 1},
+					{0, 1, 0, 0, 0, 0, 0},
+					{0, 0, 1, 0, 0, 0, 0},
+					{0, 0, 0, 1, 0, 0, 0}
+				}, 
+	
+				-- Spawns 2 large and 3 small tornades
+				{
+					{1, 0, 1, 0},
+					{0, 0, 0, 1},
+					{0, 2, 0, 0},
+					{0, 1, 0, 1}
+				}, 
+				-- Spawns 4 large tornadoes
+				{
+					{1, 2, 1},
+					{0, 0, 0},
+					{1, 0, 1}
+				}, 	
+				-- Spawns 2 small tornadoes
+				{
+					{1},
+					{1},
+					{1},
+					{2}
+				}
+			}
 
-local areaEvent6 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 0, 0},
-	{ 1, 0, 1, 2, 1, 0, 1},
-	{ 1, 0, 1, 0, 1, 0, 1},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
+-- Second set of tables shifts the spell one row to render properly.  Since the damage areas for ice tornado are always invisible in the required spots.
+-- Offsetting the value may not center the player but it keeps the correct shape.
+local EvenOrOdd ={
+				-- First table.  Spawns 3 large tornadoes and 3 smaller tornades
+				{
+					{0, 1, 0, 0, 0, 0, 0},
+					{1, 0, 0, 1, 2, 0, 1},
+					{0, 1, 0, 0, 0, 0, 0},
+					{0, 0, 1, 0, 0, 0, 0},
+					{0, 0, 0, 1, 0, 0, 0}
+				}, 
+	
+				-- Spawns 2 large and 3 small tornades
+				{
+					{1, 0, 1, 0},
+					{0, 0, 0, 1},
+					{0, 1, 2, 0},
+					{0, 1, 0, 1}
+				}, 
+				-- Spawns 4 large tornadoes
+				{
+					{1, 1, 2},
+					{0, 0, 0},
+					{1, 0, 1}
+				}, 	
+				-- Spawns 2 small tornadoes
+				{
+					{1,0},
+					{1,0},
+					{1,0},
+					{1,2}
+				}
+			}
 
-local areaEvent7 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 1, 1, 0, 0},
-	{ 1, 1, 1, 2, 1, 0, 1},
-	{ 1, 1, 1, 0, 1, 0, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
 
-local areaEvent8 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
 
-local areaEvent9 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
+local spell = Spell(SPELL_INSTANT)
+-- Loop to setup the basic combat parameters dependent on how many areas exist.
+for k,v in ipairs(MixedOddEven) do
+	combat[k] = Combat()
+	combat[k]:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)					-- Sets the damage type to ice
+	combat[k]:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)			-- Sets the correct visuals.
+	combat[k]:setArea(createCombatArea(v))										-- set the area to the combat object	
+end
 
-local areaEvent10 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 0, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent11 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent12 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 0, 0},
-	{ 1, 0, 1, 2, 1, 0, 1},
-	{ 1, 0, 1, 0, 1, 0, 1},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent13 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 1, 1, 0, 0},
-	{ 1, 1, 1, 2, 1, 0, 1},
-	{ 1, 1, 1, 0, 1, 0, 1},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
-
-local areaEvent14 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
-
-local areaEvent15 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 1, 1, 1, 2, 1, 1, 1},
-	{ 1, 1, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 0, 0, 0}
-}
-
-local areaEvent16 = {
-	{ 0, 0, 0, 0, 0, 0, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 1, 0, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent17 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 1, 0},
-	{ 1, 0, 1, 2, 1, 1, 1},
-	{ 1, 0, 1, 1, 1, 1, 1},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent18 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 0, 0},
-	{ 1, 0, 1, 2, 1, 0, 1},
-	{ 1, 0, 1, 0, 1, 0, 1},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent19 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 1, 1, 1, 1, 1, 0},
-	{ 0, 0, 1, 1, 1, 0, 0},
-	{ 0, 0, 1, 2, 1, 0, 0},
-	{ 0, 0, 1, 0, 1, 0, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-local areaEvent20 = {
-	{ 0, 0, 1, 1, 0, 0, 0},
-	{ 0, 0, 0, 1, 0, 0, 0},
-	{ 0, 0, 1, 1, 1, 0, 0},
-	{ 0, 0, 1, 2, 1, 0, 0},
-	{ 0, 0, 1, 0, 1, 0, 0},
-	{ 0, 1, 1, 0, 1, 1, 0},
-	{ 0, 0, 0, 0, 0, 0, 0}
-}
-
-combat:setArea(createCombatArea(area))
+-- Loop to setup the basic combat parameters dependent on how many areas exist.
+for k,v in ipairs(EvenOrOdd) do
+	combat[k + #MixedOddEven] = Combat()
+	combat[k + #MixedOddEven]:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)					-- Sets the damage type to ice
+	combat[k + #MixedOddEven]:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)			-- Sets the correct visuals.
+	combat[k + #MixedOddEven]:setArea(createCombatArea(v))										-- set the area to the combat object	
+end
 
 function onGetFormulaValues(player, level, magicLevel)
 	local min = (level / 5) + (magicLevel * 3.2) + 20
@@ -222,42 +110,70 @@ function onGetFormulaValues(player, level, magicLevel)
 	return min, max
 end
 
-combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+combat[1]:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
-local spell = Spell(SPELL_RUNE)
-
-function spell.onCastSpell(creature, variant, isHotkey)
-	return combat:execute(creature, variant)
+-- Function used for on event.  Used to time the combat execution to the required offset.
+local function spellAnimation(cid, variant, framNum)
+	
+	combat[framNum]:execute(cid, variant)
 end
 
-spell:name("test rune")
-spell:runeId(2275)
-spell:id(220)
-spell:level(20)
-spell:magicLevel(5)
-spell:needTarget(true)
-spell:isAggressive(false)
-spell:allowFarUse(true)
-spell:charges(25)
-spell:vocation("sorcerer;true", "master sorcerer")
-spell:register()
 
-local conjureRune = Spell(SPELL_INSTANT)
 
-function conjureRune.onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+
+function spell.onCastSpell(creature, variant)
+	-- When to refresh the large tornados.  As the large tornades have a longer animation than the smaller ones
+	-- Set the baseline reset of the loop based on that.  Since the large tornadoes do not despawn until the end of the spell.
+	local resetLargeTornadoes = 990 
+
+	-- Convert to cid to safetly pass to addEvent to correctly time when the spell effects should appear.
+	local cid = creature:getId()
+
+
+
+	-- Offset used to get the right area info. If position is the same offset the value by 4.
+	local isSame = 0
+
+		-- if x and y are both even or odd
+	local pos = Player(cid):getPosition()
+	if pos.x % 2 == pos.y % 2 then
+		isSame = 4
+	end
+
+	-- The animation for the spell is repeated 3 times sequentially through all 4 areas.
+	-- So to do that I started with the approximate timings I gathered from the video first batch of animations.
+	-- Which ended up being about 0, 300, 500, 800 milliseconds respecitivly for each area.
+	-- Than to get the reset time I got the length of the animations for the large tornadoes.
+	-- Than I got the time on how long the total time it takes for the spell to end which was 1133 ms in length. but about 990 is when it starts to shrink.
+	-- Which makes each batch of the animation a max of 990 ms.  Since going to far will make it noticibaly shrink unlike the video.
+	for i = 0, 2, 1 do	
+		addEvent(spellAnimation, 300 + resetLargeTornadoes * i, cid, variant, 2 + isSame)
+		addEvent(spellAnimation, 500 + resetLargeTornadoes * i, cid, variant, 3 + isSame)
+		addEvent(spellAnimation, 800 + resetLargeTornadoes * i, cid, variant, 4 + isSame)
+	end
+
+
+	-- Not in the loop since it needs to be called immediatly to activate the spell immediatly and to say the key phrase.
+	-- Making the addEvent say 0 starts the combat execution the next frame which desyncs the spell words from showing at the same time as the spell.
+	-- With the first combat area is already off by one.  Put in the extra events outside the loop to prevent needing an extra if statement in the for loop.
+
+	addEvent(spellAnimation, resetLargeTornadoes, cid, variant, 1 + isSame)
+	addEvent(spellAnimation, (resetLargeTornadoes*2), cid, variant, 1 + isSame)
+	return combat[1 + isSame]:execute(cid, variant)
 end
 
-conjureRune:name("Test")
-conjureRune:id(221)
-conjureRune:words("frigo3")
-conjureRune:level(0)
-conjureRune:mana(0)
-conjureRune:group("support")
-conjureRune:soul(0)
-conjureRune:isAggressive(false)
-conjureRune:cooldown(2000)
-conjureRune:groupCooldown(2000)
-conjureRune:needLearn(false)
-conjureRune:vocation("druid", "master sorcerer")
-conjureRune:register()
+
+-- Required values to setup the spell.
+spell:name("Winter Storm") -- Spell name shown in spell book.
+spell:id(221)			   -- spellid
+spell:words("frigo")	   -- keywords used to cast the spell.
+spell:level(0)			   -- Set level to 0 to ensure anyone can cast it.
+spell:mana(0)			   -- Set mana cost to 0 to easily test it.
+spell:group("attack")      -- Spell seemed similar to the attack spells in the game so gave it to the attack group.
+spell:soul(0)
+spell:cooldown(2000)	   -- Spell cooldown.  Made it 2000 so I dont accidently use it again while testing the spell
+spell:groupCooldown(2000)  
+spell:needLearn(false)	   -- Set to false so you dont need to go learn the spell.
+spell:vocation("druid")    -- All the ice spells are in the druid vocation.
+
+spell:register()		   -- Register the spell to be used in the game.
